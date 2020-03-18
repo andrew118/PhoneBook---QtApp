a5878c160044ee31ce68ca2d70623682b3bbf284
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-03-18 10:35:28 +0100

    Prepared class variables and functions to connect DB

--- connectiondb.cpp
+++ connectiondb.cpp
@@ -2,5 +2,16 @@
 
 connectionDB::connectionDB()
 {
+    connectDB();
+}
 
+void connectionDB::connectDB()
+{
+    db = QSqlDatabase::addDatabase(DRIVER);
+    db.setDatabaseName(HOST);
+    db.setUserName(USER);
+    db.setPassword(PASSWORD);
+    db.setPort(PORT);
+
+    qDebug() << "DB status: " << db.open();
 }
--- connectiondb.h
+++ connectiondb.h
@@ -1,11 +1,24 @@
 #ifndef CONNECTIONDB_H
 #define CONNECTIONDB_H
 
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QDebug>
 
 class connectionDB
 {
 public:
     connectionDB();
+
+private:
+    QSqlDatabase db;
+    const QString DRIVER = "QMYSQL";
+    const QString HOST = "localhost";
+    const QString PASSWORD = "";
+    const QString USER = "root";
+    const int PORT = 3306;
+
+    void connectDB();
 };
 
 #endif // CONNECTIONDB_H
